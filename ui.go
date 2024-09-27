1402a553b0c1c7cdbfa1905f5be20484d61f6d0f
jankampherbeek <tech@kampherbeek.com>
2024-09-27 13:39:39 +0200

    feat: (frontend) UI with resizeable circle as placeholder for chart.

--- ui.go
+++ ui.go
@@ -1,49 +1,59 @@
-/*
- *  Enigma Astrology Research.
- *  Copyright (c) Jan Kampherbeek.
- *  Enigma is open source.
- *  Please check the file copyright.txt in the root of the source for further details.
- */
-
 package main
 
 import (
 	"fmt"
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 	"image/color"
 )
 
-func makeHeader() *widget.Label {
-	aTitle := "Enigma Astrology Research - version 1.0"
-	return widget.NewLabelWithStyle(aTitle, fyne.TextAlignCenter, fyne.TextStyle{
-		Bold:      true,
-		Italic:    false,
-		Monospace: false,
-		Symbol:    false,
-		TabWidth:  0,
-	})
-
+type Circle struct {
+	widget.BaseWidget
 }
 
-func makeButton() *widget.Button {
-	b := widget.NewButton("Tutorial",
-		func() {
-			showTutorial("Please study....")
-		})
-	b.Importance = widget.HighImportance
-	return b
-
+func NewCircle() *Circle {
+	c := &Circle{}
+	c.ExtendBaseWidget(c)
+	return c
 }
 
-func showTutorial(text string) {
-	fmt.Println(text)
+func (c *Circle) CreateRenderer() fyne.WidgetRenderer {
+	circle := canvas.NewCircle(color.NRGBA{R: 0, G: 0, B: 255, A: 255})
+	return &circleRenderer{circle: circle}
 }
 
-func makeBoxOriginal() *fyne.Container {
+type circleRenderer struct {
+	circle *canvas.Circle
+}
+
+func (r *circleRenderer) Layout(size fyne.Size) {
+	r.circle.Resize(size)
+}
+
+func (r *circleRenderer) MinSize() fyne.Size {
+	return fyne.NewSize(50, 50) // Minimum size for the circle
+}
+
+func (r *circleRenderer) Refresh() {
+	canvas.Refresh(r.circle)
+}
+
+func (r *circleRenderer) BackgroundColor() color.Color {
+	return theme.BackgroundColor()
+}
+
+func (r *circleRenderer) Objects() []fyne.CanvasObject {
+	return []fyne.CanvasObject{r.circle}
+}
+
+func (r *circleRenderer) Destroy() {}
+
+func makeBox() *fyne.Container {
 	//header := makeHeader()
 	button1 := makeButton()
 	button2 := widget.NewButton("Charts", func() { fmt.Println("Charts clicked.") })
@@ -62,36 +72,34 @@ func makeBoxOriginal() *fyne.Container {
 
 	text2 := canvas.NewText("Text 2", color.Black)
 	text2.TextSize = 36
-	//	text3 := canvas.NewText("Text 3", color.Black)
-	//	data := widget.NewEntry()
 	enigmaHead := canvas.NewText("Enigma 1.0", color.Black)
 	enigmaHead.TextSize = 36
 	labelGlobalBtns := widget.NewLabel("Modules")
 	labelGlobalBtns.Importance = widget.MediumImportance
 	labelLocalBtns := widget.NewLabel("Charts")
 	labelLocalBtns.Importance = widget.MediumImportance
-	//circle := canvas.NewCircle(color.RGBA{R: 126, G: 126, B: 255, A: 255})
-	//circle.Resize(fyne.NewSize(600, 600))
-	//content := container.NewWithoutLayout(circle)
-	//circle := NewCircle()
-	//content := container.NewMax(circle)
+	labelDescription := widget.NewLabel("Description of chart: namew, date, time, coordinates")
+	labelDescription.Importance = widget.MediumImportance
 	boxGlobal := container.New(layout.NewVBoxLayout(), enigmaHead, labelGlobalBtns, button1, button2, button3, button4, button5)
 	boxLocal := container.New(layout.NewVBoxLayout(), labelLocalBtns, btnNewChart, btnSearchChart)
-
 	leftBox := container.New(layout.NewVBoxLayout(), boxGlobal, boxLocal)
-	//	grid := container.New(layout.NewGridLayout(2), header, label1, text2, text3, data)
-	totalBox := container.New(layout.NewHBoxLayout(), leftBox, boxLocal)
-	return totalBox
-
+	return leftBox
 }
 
-func main() {
-
-	handleUi()
-	/*	a := app.New()
-		w := a.NewWindow("Enigma")
-		w.Resize(fyne.NewSize(1024, 768))
-		w.SetContent(makeBox())
-		w.ShowAndRun()*/
-
+func handleUi() {
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Resizable Circle")
+	labelDescription := widget.NewLabel("Description of chart: name, date, time, coordinates")
+	labelDescription.Importance = widget.MediumImportance
+	labelSettings := widget.NewLabel("Placeholder for settings: housesystem, parallax, ayanamsha bodies included etc.")
+	circle := NewCircle()
+	content := container.NewBorder(
+		labelDescription,
+		labelSettings,
+		makeBox(),
+		nil,
+		circle)
+	myWindow.SetContent(content)
+	myWindow.Resize(fyne.NewSize(900, 700))
+	myWindow.ShowAndRun()
 }
